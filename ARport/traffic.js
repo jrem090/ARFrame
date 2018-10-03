d6d1ed883f1bd65221959ea1e19bb2219381fd6e
John Remmes <john.remmes@richmond.edu>
2018-10-03 08:45:55 -0400

    Update traffic.js

--- ARport/traffic.js
+++ ARport/traffic.js
@@ -13,7 +13,8 @@ function myFunction(arr) {
 function requestTraffic()
 {
         var xmlhttp = new XMLHttpRequest();
-    var url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100"
+    //var url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100"
+    var url = "https://adsbexchange.com/api/aircraft/json/lat/33.433638/lon/-112.008113/dist/10/";
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
