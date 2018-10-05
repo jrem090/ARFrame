987259034ef073bef1f07b0a11ecdaa81df0950c
John Remmes <john.remmes@richmond.edu>
2018-10-04 20:07:00 -0400

    1`21

--- ARport/traffic.js
+++ ARport/traffic.js
@@ -15,15 +15,14 @@ function requestTraffic()
     var xmlhttp = new XMLHttpRequest();
     var url = "https://adsbexchange.com/api/aircraft/json/lat/33.433638/lon/-112.008113/dist/10/"
 
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.setRequestHeader('api-auth', 'test')
-    xmlhttp.send();
-
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
             myFunction(myArr);
         }
     };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.setRequestHeader('api-auth', 'test')
+    xmlhttp.send();
 }
